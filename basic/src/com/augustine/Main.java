8487eec31fa0d70f2127538061da61ba08d5db33
augustine <augustino0890@gmail.com>
2022-02-20 15:17:03 +0900

    java: scanner method

--- basic/src/com/augustine/Main.java
+++ basic/src/com/augustine/Main.java
@@ -1,10 +1,20 @@
 package com.augustine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
         System.out.println("My name is Augustine");
+
+        String greeting = "Hi, ";
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        System.out.println(greeting + text);
+
+        Double num = scanner.nextDouble();
+        System.out.println(num);
     }
 }
---
+++ basic/src/com/augustine/MathOperations.java
@@ -0,0 +1,4 @@
+package com.augustine;
+
+public class MathOperations {
+}
