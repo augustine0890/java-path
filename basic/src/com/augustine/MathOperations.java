f13d4e39a45b16b575478a7fd5e997e8f4605dec
augustine <augustino0890@gmail.com>
2022-02-20 15:25:34 +0900

    java: basic operations

--- basic/src/com/augustine/MathOperations.java
+++ basic/src/com/augustine/MathOperations.java
@@ -1,4 +1,24 @@
 package com.augustine;
 
 public class MathOperations {
+    public static void main(String[] args) {
+        int three = 3;
+        int six = 6;
+        int add = three + six;
+        System.out.println(add);
+        System.out.println(six - three);
+        System.out.println(11 % 3);
+        System.out.println(11 / 3);
+
+        // Comparison Operators
+        int myAge = 32;
+        int yourAge = 29;
+        String name = "Augustine";
+        System.out.println(myAge > yourAge);
+        System.out.println(myAge == yourAge);
+        System.out.println(myAge >= yourAge);
+        System.out.println(myAge != yourAge);
+        System.out.println(myAge <= yourAge);
+        System.out.println("Name contains 'A' letter: " + name.contains("A"));
+    }
 }
