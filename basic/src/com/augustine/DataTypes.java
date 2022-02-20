32aac7c3172da64c7dd4c901b7e9d2c8659edf4e
augustine <augustino0890@gmail.com>
2022-02-20 14:55:13 +0900

    java: data types

---
+++ basic/src/com/augustine/DataTypes.java
@@ -0,0 +1,20 @@
+package com.augustine;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        char name = 'A';
+        String fullName = "Augustine";
+        System.out.println(name);
+        System.out.println(fullName);
+
+        boolean isRight = true;
+        System.out.println(isRight);
+
+        short theShort = 12_234;
+        int number = 34_4354;
+        long theLong = 32_234_2345;
+        System.out.println(theLong);
+        double doub = 233.5235;
+        System.out.println(doub);
+    }
+}
