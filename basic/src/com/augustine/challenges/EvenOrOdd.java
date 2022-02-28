e83d4a0257a4235e4cafc8cd1a1941dad0a14049
augustine <augustino0890@gmail.com>
2022-02-28 21:59:49 +0900

    java-chellanges: isEven2

--- basic/src/com/augustine/challenges/EvenOrOdd.java
+++ basic/src/com/augustine/challenges/EvenOrOdd.java
@@ -7,10 +7,23 @@ public class EvenOrOdd {
         return n % 2 == 0;
         // return (n & 1) == 0;
     }
+
+    public static boolean isEven2(int n) {
+        while (n > 2) {
+            n -= 2;
+        }
+        if (n == 2 || n == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int userNum = scanner.nextInt();
         System.out.println("Is the number even? " + isEven(userNum));
+        System.out.println("Is the number even? " + isEven2(userNum));
     }
 }
+
