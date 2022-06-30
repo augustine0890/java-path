2a879743a5fec850f3dff73c463802b9d8c5823a
augustine <augustino0890@gmail.com>
2022-06-30 18:20:08 +0900

    java: sort array

--- basic/src/com/augustine/Array.java
+++ basic/src/com/augustine/Array.java
@@ -1,13 +1,18 @@
 package com.augustine;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Array {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {5, 3, 1, 4, 2};
+        Arrays.sort(numbers);
         for (int i=0; i<=numbers.length-1; i++) {
             System.out.println(numbers[i]);
         }
 
-        String[] names = {"Adam", "David", "Leo", "Tom"};
+        String[] names = {"Tom", "Sam", "Leo", "Adam", "David"};
+        Collections.sort(Arrays.asList(names));
         // for-each loop
         for (String n: names) {
             System.out.println(n);
