20ae2e62ea4de16bac5de8a0a8da878f846af453
augustine <augustino0890@gmail.com>
2022-07-04 15:54:58 +0900

    java: exceptions

---
+++ basic/src/com/augustine/Exceptions.java
@@ -0,0 +1,23 @@
+package com.augustine;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[5]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e);
+        }
+
+        checkAge(28);
+        checkAge(15);
+    }
+
+    public static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+}
