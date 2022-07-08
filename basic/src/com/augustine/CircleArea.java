b0fbc6b85ba166ed20ceef7e544216505923a952
augustine <augustino0890@gmail.com>
2022-07-08 17:30:42 +0900

    basic: fractional values of numbers

---
+++ basic/src/com/augustine/CircleArea.java
@@ -0,0 +1,11 @@
+package com.augustine;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        double r, pi, area;
+        pi = 3.141592654;
+        r = 6.0;
+        area = pi * r * r;
+        System.out.println(area);
+    }
+}
