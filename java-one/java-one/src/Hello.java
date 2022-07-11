521f9e242601f8c3b5d49161c106fc704908f919
augustine <augustino0890@gmail.com>
2022-07-11 12:15:53 +0900

    java-one: print out line

---
+++ java-one/java-one/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Jane Done");
+        System.out.println("Hello world");
+        System.out.println("(And all the people of the world)");
+        System.out.println(
+                "    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******\n" +
+                "*********\n" +
+                "    *");
+    }
+}
