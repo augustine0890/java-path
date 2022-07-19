84eee0f911c29d25480fc58bb5bf5b4ef18404e7
augustine <augustino0890@gmail.com>
2022-07-19 12:11:10 +0900

    ds: binary search tree (insertion)

--- data-structures/src/BinarySearchTree/BinarySearchTree.java
+++ data-structures/src/BinarySearchTree/BinarySearchTree.java
@@ -25,4 +25,44 @@ public class BinarySearchTree<T extends Comparable<T>> {
     public int size() {
         return nodeCount;
     }
+
+    // Returns true if successfully inserted
+    public boolean add(T elem) {
+        if (contains(elem)) {
+            return false;
+        } else {
+            root = add(root, elem);
+            nodeCount++;
+            return true;
+        }
+    }
+
+    // Private method to add a value in the Binary Tree
+    private Node add(Node node, T elem) {
+        if (node == null) {
+            node = new Node(null, null, elem);
+        } else {
+            if (elem.compareTo(node.data) < 0) {
+                node.left = add(node.left, elem);
+            } else {
+                node.right = add(node.right, elem);
+            }
+        }
+        return node;
+    }
+
+    // Returns true is the element exists in the tree
+    public boolean contains(T elem) {
+        return contains(root, elem);
+    }
+
+    // Find an element in the tree
+    private boolean contains(Node node, T elem) {
+        if (node == null) return false;
+
+        int cmp = elem.compareTo(node.data);
+        if (cmp < 0) return contains(node.left, elem);
+        else if (cmp > 0) return contains(node.right, elem);
+        else return true;
+    }
 }
